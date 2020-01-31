445d7e2590c8d66c7be0c7cda99002e787046ae4
saher Noureen <sahernoureen1@gmail.com>
2020-01-31 09:47:06 -0600

    Updates

---
+++ precache-manifest.2c043139f89832a620b9dad648e2d0e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b28ea08cd0bad3e38a60fb544df6531",
+    "url": "/React-CloneSocialMediaNewsFeed/index.html"
+  },
+  {
+    "revision": "e8b5b8de0ad70732f257",
+    "url": "/React-CloneSocialMediaNewsFeed/static/css/2.1a08bad1.chunk.css"
+  },
+  {
+    "revision": "fb368e3969005d6a98f7",
+    "url": "/React-CloneSocialMediaNewsFeed/static/css/main.4c5fe687.chunk.css"
+  },
+  {
+    "revision": "e8b5b8de0ad70732f257",
+    "url": "/React-CloneSocialMediaNewsFeed/static/js/2.8cd2d239.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-CloneSocialMediaNewsFeed/static/js/2.8cd2d239.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fb368e3969005d6a98f7",
+    "url": "/React-CloneSocialMediaNewsFeed/static/js/main.6946167e.chunk.js"
+  },
+  {
+    "revision": "297d83be747c62784ec1",
+    "url": "/React-CloneSocialMediaNewsFeed/static/js/runtime-main.76096259.js"
+  },
+  {
+    "revision": "082ab8643cd173964e29afa99431324f",
+    "url": "/React-CloneSocialMediaNewsFeed/static/media/pic2.082ab864.jpg"
+  }
+]);
